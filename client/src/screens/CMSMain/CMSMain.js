47cf444fa8838bf0aa6b06c39987bf7e0350a18f
Dushyant Yadav <dushyant9309@gmail.com>
2020-12-28 18:50:44 +0530

    Add ControlBar + some cleanup

--- client/src/screens/CMSMain/CMSMain.js
+++ client/src/screens/CMSMain/CMSMain.js
@@ -1,6 +1,8 @@
 import React, { useEffect, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
+import { MdFontDownload, MdSettings } from 'react-icons/md'
+import { FaPlus } from 'react-icons/fa'
 
 import SideNav from '../../components/SideNav/SideNav'
 import ContentControl from '../../components/ContentControl/ContentControl'
@@ -8,6 +10,8 @@ import ContentControl from '../../components/ContentControl/ContentControl'
 import * as actions from '../../store/actions/index'
 
 import './CMSMain.css'
+import ControlBar from '../../components/ControlBar/ControlBar'
+
 
 const CMSMain = (props) => {
     // Load the root PageGroup for the user
@@ -17,6 +21,20 @@ const CMSMain = (props) => {
         }
     }, [props.token])
 
+    let controls = []
+    const addControl = (
+        <div className='CMSMain__ControlBtn'>
+            <FaPlus/>
+        </div>
+    )
+    const settingsControl = (
+        <div className='CMSMain__ControlBtn'>
+            <MdSettings/>
+        </div>
+    )
+    controls.push(addControl)
+    controls.push(settingsControl)
+
     console.log('bruhhhhh')
     return (
         <div className='CMSMain'>
@@ -24,6 +42,9 @@ const CMSMain = (props) => {
             <div className="MainArea">
                 <ContentControl/>
             </div>
+            <ControlBar>
+                {controls}
+            </ControlBar>
         </div>
     )
 }
